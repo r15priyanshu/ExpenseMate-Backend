68932a39fffde187c09b6d2798185a6b98ec6f34
Priyanshu Anand <anandpriyanshu6@gmail.com>
2025-01-25 17:42:33 +0530

    Initial Project Setup

---
+++ src/main/java/com/anshuit/expensemate/ExpensemateApplication.java
@@ -0,0 +1,13 @@
+package com.anshuit.expensemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensemateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensemateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anshuit/expensemate/ExpensemateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anshuit.expensemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensemateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
